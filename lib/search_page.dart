a8da288f22cd0b509de7320fec3e9dfdd60fb1ba
Lee bosung <dlqh406@gmail.com>
2020-08-15 03:55:04 +0900

    searchpage에 firestore데이터를  불러오늘 방법 path, documents의 뜻과 사용법 ,item builder 이해

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'create_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'detail_post_page.dart';
 
 class SearchPage extends StatelessWidget {
@@ -28,16 +29,35 @@ class SearchPage extends StatelessWidget {
   Widget _buildBody(context) {
     print('search_page created');
     return Scaffold(
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            childAspectRatio: 1.0,
-            mainAxisSpacing: 1.0,
-            crossAxisSpacing: 1.0),
-        itemCount: 3,
-        itemBuilder: (BuildContext context, int index) {
-          return _buildListItem();
-        },
+      // firestore에서 특정하여 데이터를 가져올 경우, QuerySnapshot 을 가져옴
+      body: StreamBuilder<QuerySnapshot>(
+        // path('post')는 왼쪽에서 첫번째
+        stream: Firestore.instance.collection('post').snapshots(),
+        // stream을 통해서 path가 'post'인 데이터 전체가 아래 snapshot으로 들어감
+        builder: (context, snapshot) {
+          if(!snapshot.hasData){
+            // 데이터가 없으면 progressbar 반복
+            return Center(child:  CircularProgressIndicator(),);
+          }
+          print("data: ");
+          print(snapshot.data.documents.length);
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                // 가로 세로 비율
+                childAspectRatio: 1.0,
+                // grid간 간격
+                mainAxisSpacing: 1.0,
+                crossAxisSpacing: 1.0),
+            // documents는 firestore 에서 왼쪽에서 3번째
+            // 데이터가 다 들어가있는 리스트 [{text, greentea, test@test.com, asadsad},{...}]
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _buildListItem(snapshot.data.documents[index]);
+
+            },
+          );
+        }
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blueAccent,
@@ -51,12 +71,9 @@ class SearchPage extends StatelessWidget {
     );
   }
 
-  Widget _buildListItem() {
-    return Image.network(
-      '',
+  Widget _buildListItem(DocumentSnapshot document) {
+    return Image.network(document['photoUrl'],
       fit: BoxFit.cover,
     );
   }
 }
-
-//fireStore에 데이터 수동으로 추가했음
