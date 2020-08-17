65060bae690a2cf3814dded0afecf022642e5382
Lee bosung <dlqh406@gmail.com>
2020-08-17 11:53:04 +0900

    accountpage에  searchpage의 streambuilder  옮기기

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -1,3 +1,4 @@
+import 'package:chapter10/detail_post_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -14,149 +15,187 @@ class AccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: _buildBody(),
+      body: _buildBody(context),
     );
   }
 
-  Widget _buildBody() {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
+  Widget _buildBody(context) {
+    return Column(
         children: <Widget>[
           _buildProfile(),
+          Padding(padding: EdgeInsets.all(10)),
+          _buildGrid(context)
+        ],
+      );
+  }
+
+  Widget _buildProfile() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  SizedBox(
+                    width: 80.0,
+                    height: 80.0,
+                    child: GestureDetector(
+                      onTap: () => print('이미지 클릭'),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(user.photoUrl),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    height: 80.0,
+                    alignment: Alignment.bottomRight,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 28.0,
+                          height: 28.0,
+                          child: FloatingActionButton(
+                            onPressed: null,
+                            backgroundColor: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 25.0,
+                          height: 25.0,
+                          child: FloatingActionButton(
+                            backgroundColor: Colors.blue,
+                            onPressed: null,
+                            child: Icon(Icons.add),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+              ),
+              Text(
+                user.displayName,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            // stream메소드의 < >안에있는 파일 형식 맞추기 : QuerySnapshot
+            child: StreamBuilder<QuerySnapshot>(
+              stream: _postStream(),
+              builder: (context, snapshot) {
+                var post = 0;
+                if (snapshot.hasData){
+                  // documents => 맨 오른쪽 데이터
+                  // snapshot.data.documents.length -> documents 리스트의 길이
+                  post = snapshot.data.documents.length;
+                }
+                return Text(
+                  '$post\n게시물',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18.0),
+                );
+              }
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: StreamBuilder<DocumentSnapshot>(
+              stream: _followerStream(),
+              builder: (context, snapshot) {
+                var follower =0;
+                if(snapshot.hasData){
+                  var filteredMap;
+                  // snapdata.data.data => 맨 오른쪽 데이터
+                  if(snapshot.data.data == null){
+                    filteredMap = [];
+                  } else {
+                    filteredMap = snapshot.data.data
+                      ..removeWhere((key, value) => value == false);
+                  }follower = filteredMap.length;
+                }
+                return Text(
+                  '$follower\n팔로워',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18.0),
+                );
+              }
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: StreamBuilder<DocumentSnapshot>(
+              stream: _followingStream(),
+              builder: (context, snapshot) {
+                var following =0;
+                if(snapshot.hasData){
+                  var filteredMap;
+                  if(snapshot.data.data == null){
+                    filteredMap = [];
+                  } else {
+                    //..removeWhere: ~(뒤조건) 빼고 리스트에 넣는 메소드 .removeWhere에 불들어오면.하나더
+                    filteredMap = snapshot.data.data
+                      ..removeWhere((key, value) => value == false);
+                  }following = filteredMap.length;
+                }
+                return Text(
+                  '$following\n팔로잉',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18.0),
+                );
+              }
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildProfile() {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                SizedBox(
-                  width: 80.0,
-                  height: 80.0,
-                  child: GestureDetector(
-                    onTap: () => print('이미지 클릭'),
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(user.photoUrl),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 80.0,
-                  height: 80.0,
-                  alignment: Alignment.bottomRight,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                        width: 28.0,
-                        height: 28.0,
-                        child: FloatingActionButton(
-                          onPressed: null,
-                          backgroundColor: Colors.white,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 25.0,
-                        height: 25.0,
-                        child: FloatingActionButton(
-                          backgroundColor: Colors.blue,
-                          onPressed: null,
-                          child: Icon(Icons.add),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-            ),
-            Text(
-              user.displayName,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-            )
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          // stream메소드의 < >안에있는 파일 형식 맞추기 : QuerySnapshot
-          child: StreamBuilder<QuerySnapshot>(
-            stream: _postStream(),
+
+  Widget _buildGrid(context) {
+    print('search_page created');
+    return Expanded(
+      child: StreamBuilder<QuerySnapshot>(
+          // path('post')는 왼쪽에서 첫번째
+            stream: Firestore.instance.collection('post').snapshots(),
+            // stream을 통해서 path가 'post'인 데이터 전체가 아래 snapshot으로 들어감
             builder: (context, snapshot) {
-              var post = 0;
-              if (snapshot.hasData){
-                // documents => 맨 오른쪽 데이터
-                // snapshot.data.documents.length -> documents 리스트의 길이
-                post = snapshot.data.documents.length;
+              if(!snapshot.hasData){
+                // 데이터가 없으면 progressbar 반복
+                return Center(child:  CircularProgressIndicator(),);
               }
-              return Text(
-                '$post\n게시물',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18.0),
+              print("data: ");
+              print(snapshot.data.documents.length);
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    // 가로 세로 비율
+                    childAspectRatio: 1.0,
+                    // grid간 간격
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0),
+                // documents는 firestore 에서 왼쪽에서 3번째
+                // 데이터가 다 들어가있는 리스트 [{text, greentea, test@test.com, asadsad},{...}]
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return _buildListItem(context, snapshot.data.documents[index]);
+
+                },
               );
             }
-          ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: StreamBuilder<DocumentSnapshot>(
-            stream: _followerStream(),
-            builder: (context, snapshot) {
-              var follower =0;
-              if(snapshot.hasData){
-                var filteredMap;
-                // snapdata.data.data => 맨 오른쪽 데이터
-                if(snapshot.data.data == null){
-                  filteredMap = [];
-                } else {
-                  filteredMap = snapshot.data.data
-                    ..removeWhere((key, value) => value == false);
-                }follower = filteredMap.length;
-              }
-              return Text(
-                '$follower\n팔로워',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18.0),
-              );
-            }
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: StreamBuilder<DocumentSnapshot>(
-            stream: _followingStream(),
-            builder: (context, snapshot) {
-              var following =0;
-              if(snapshot.hasData){
-                var filteredMap;
-                if(snapshot.data.data == null){
-                  filteredMap = [];
-                } else {
-                  //..removeWhere: ~(뒤조건) 빼고 리스트에 넣는 메소드 .removeWhere에 불들어오면.하나더
-                  filteredMap = snapshot.data.data
-                    ..removeWhere((key, value) => value == false);
-                }following = filteredMap.length;
-              }
-              return Text(
-                '$following\n팔로잉',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18.0),
-              );
-            }
-          ),
-        ),
-      ],
     );
   }
 
@@ -208,5 +247,22 @@ Stream<DocumentSnapshot> _followingStream(){
         .collection('follower')
         .document(user.email)
         .snapshots();
+  }
+
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return Hero(
+      tag: document.documentID,
+      child: Material(
+        child: InkWell(
+          onTap: (){
+            Navigator.push(
+              context, MaterialPageRoute(builder: (context) => DetailPostPage(document, user)),
+            );
+          },
+          child: document['email'] == user.email?Image.network(document['photoUrl'],fit: BoxFit.cover):null
+          ),
+        ),
+      );
   }
 }
